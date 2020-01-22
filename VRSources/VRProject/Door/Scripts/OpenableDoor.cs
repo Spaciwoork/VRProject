48cb8dbaa85664a8022819cdc5fc750d012a71bc
Vengadesan Sridhar <vsridhar@localhost.localdomain>
2020-01-22 11:06:34 +0100

    add scene

---
+++ VRSources/VRProject/Door/Scripts/OpenableDoor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenableDoor : MonoBehaviour
+{
+    public GameObject Door;
+    Animator Anim;
+
+    void Start()
+    {
+        Anim = Door.GetComponent<Animator>();
+    }
+
+    // Main function
+    void Update()
+    {
+
+    }
+
+    // Display a simple info message when player is inside the trigger area
+    void OnGUI()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        //Debug.Log("Trigger Enter");
+        Anim.SetTrigger("OpenDoor");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Trigger Exit");
+        Anim.enabled = true;
+    }
+}
---
+++ VRSources/VRProject/Door/Scripts/OpenableDoorPause.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenableDoorPause : MonoBehaviour
+{
+    public GameObject Door;
+    public Animator Anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anim = Door.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void pauseEvent()
+    {
+        Debug.Log("Pause");
+        Anim.enabled = false;
+    }
+}
